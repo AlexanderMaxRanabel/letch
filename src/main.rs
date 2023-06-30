fb01c4fe3ece380b303aaf659fe84e8faef33185
Voyage <alpmimioglu111@gmail.com>
2023-06-30 16:59:05 +0300

    ınıt

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+
+fn distro_name_get() -> String {
+    //most of the code comes from my other project
+    if let Ok(file) = File::open("/etc/os-release") {
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                if line.starts_with("ID=nixos") {
+                    return "NixOS".to_string();
+                } else if line.starts_with("ID=arch") {
+                    return "Arch Linux".to_string();
+                } else if line.starts_with("ID=debian") {
+                    return "Debian".to_string();
+                } else if line.starts_with("ID=fedora") {
+                    return "Fedora".to_string();
+                }
+            }
+        }
+    }
+    return "Linux".to_string();
+}
+
+fn hostname_get() -> String {
+    let mut file = File::open("/etc/hostname").expect("Failed to open file");
+    let mut hostname = String::new();
+    file.read_to_string(&mut hostname).expect("Failed to read the file");
+
+    return hostname;
+}
+
+fn main() {
+    let distro_name = distro_name_get();
+    let hostname = hostname_get();
+    println!("Distro: {}", distro_name);
+    println!("Hostname: {}", hostname);
+}
